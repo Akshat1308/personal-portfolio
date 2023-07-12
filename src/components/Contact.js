a8c008cdf3714f598f97596c234aec19ddf1eec4
Akshat Surana <suranaas@rknec.edu>
2023-07-12 16:36:51 +0530

    .

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,59 +9,40 @@ import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 import emailjs from "@emailjs/browser";
 import styled from "styled-components";
+
 export const Contact = () => {
   const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
   const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
+  const [buttonText, setButtonText] = useState("Send");
   const [status, setStatus] = useState({});
+  const [isMessageSent, setMessageSent] = useState(false);
 
   const onFormUpdate = (category, value) => {
     setFormDetails({
       ...formDetails,
-      [category]: value
-    })
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
+      [category]: value,
     });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully' });
-    } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
-    }
   };
-
-
-  ///////////////////////////////////////////////
-  const form = useRef();
-  const [isMessageSent, setMessageSent] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
+    if (!isFormValid()) {
+      return;
+    }
+
     setButtonText("Sending...");
 
     emailjs
       .sendForm(
         "service_uxz0t9b",
         "template_8byyk6d",
-        form.current,
+        e.target,
         "1eX6a-z7Ag1IVKlTg"
       )
       .then(
@@ -70,7 +51,7 @@ export const Contact = () => {
           setMessageSent(true);
           setButtonText("Send");
 
-          form.current.reset();
+          setFormDetails(formInitialDetails);
 
           // Hide the message after 3 seconds
           setTimeout(() => {
@@ -80,9 +61,26 @@ export const Contact = () => {
         (error) => {
           console.log(error.text);
           setButtonText("Send");
-
         }
       );
+  };
+
+  const isFormValid = () => {
+    const { firstName, lastName, email, phone, message } = formDetails;
+    if (
+      firstName.trim() === "" ||
+      lastName.trim() === "" ||
+      email.trim() === "" ||
+      phone.trim() === "" ||
+      message.trim() === ""
+    ) {
+      setStatus({
+        success: false,
+        message: "Please fill in all fields.",
+      });
+      return false;
+    }
+    return true;
   };
 
   useEffect(() => {
@@ -97,73 +95,114 @@ export const Contact = () => {
     return () => clearTimeout(timeout);
   }, [isMessageSent]);
 
-  /////////////////////////////////////////////////////////////////////////////
-
   return (
     <section className="contact" id="connect">
       <Container>
         <Row className="align-items-center">
           <Col size={12} md={6}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" style={{ height: '500px', width: '500px' }} />
-              }
+              {({ isVisible }) => (
+                <img
+                  className={isVisible ? "animate__animated animate__zoomIn" : ""}
+                  src={contactImg}
+                  alt="Contact Us"
+                  style={{ height: "500px", width: "500px" }}
+                />
+              )}
             </TrackVisibility>
           </Col>
           <Col size={12} md={6}>
             <TrackVisibility>
-              {({ isVisible }) =>
+              {({ isVisible }) => (
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <h2>Get In Touch</h2>
-                  <form ref={form} onSubmit={sendEmail}>                    <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" name="user_name" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" name="user_last" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" name="user_email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" name="user_phone" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea name="message" rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit" value="Send"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                  <form onSubmit={sendEmail}>
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="text"
+                          name="user_name"
+                          value={formDetails.firstName}
+                          placeholder="First Name"
+                          onChange={(e) => onFormUpdate("firstName", e.target.value)}
+                        />
                       </Col>
-                    }
-                  </Row>
-                  {isMessageSent && <MessageSentPopup>Message Sent!</MessageSentPopup>}
-
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="text"
+                          name="user_last"
+                          value={formDetails.lasttName}
+                          placeholder="Last Name"
+                          onChange={(e) => onFormUpdate("lastName", e.target.value)}
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="email"
+                          name="user_email"
+                          value={formDetails.email}
+                          placeholder="Email Address"
+                          onChange={(e) => onFormUpdate("email", e.target.value)}
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="tel"
+                          name="user_phone"
+                          value={formDetails.phone}
+                          placeholder="Phone No."
+                          onChange={(e) => onFormUpdate("phone", e.target.value)}
+                        />
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea
+                          name="message"
+                          rows="6"
+                          value={formDetails.message}
+                          placeholder="Message"
+                          onChange={(e) => onFormUpdate("message", e.target.value)}
+                        ></textarea>
+                        <button type="submit" value="Send">
+                          <span>{buttonText}</span>
+                        </button>
+                      </Col>
+                      {status.message && (
+                        <Col>
+                          <p className={status.success === false ? "danger" : "success"}>
+                            {status.message}
+                          </p>
+                        </Col>
+                      )}
+                    </Row>
                   </form>
-                </div>}
+                  {isMessageSent && <MessageSentPopup>Message Sent!</MessageSentPopup>}
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
-        <Container >
+        <Container>
           <Row className="align-items-center">
-            {/* <MailchimpForm /> */}
-            <Col size={12} sm={6} className="text-center" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
-              <h3 style={{ marginBottom: '25px', marginTop: '10vh' }}>Connect with me on</h3>
+            <Col size={12} sm={6} className="text-center" style={{ marginLeft: "auto", marginRight: "auto" }}>
+              <h3 style={{ marginBottom: "25px", marginTop: "10vh" }}>Connect with me on</h3>
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/akshat-surana-491b74217/"><img src={navIcon1} alt="Icon" /></a>
-                <a href="https://github.com/Akshat1308"><img src={navIcon2} alt="Icon" style={{ height: '30px', width: '30px' }} /></a>
-                <a href="https://instagram.com/akshatsurana001?igshid=MmIzYWVlNDQ5Yg=="><img src={navIcon3} alt="Icon" /></a>
+                <a href="https://www.linkedin.com/in/akshat-surana-491b74217/">
+                  <img src={navIcon1} alt="Icon" />
+                </a>
+                <a href="https://github.com/Akshat1308">
+                  <img src={navIcon2} alt="Icon" style={{ height: "30px", width: "30px" }} />
+                </a>
+                <a href="https://instagram.com/akshatsurana001?igshid=MmIzYWVlNDQ5Yg==">
+                  <img src={navIcon3} alt="Icon" />
+                </a>
               </div>
             </Col>
           </Row>
         </Container>
       </Container>
     </section>
-  )
-}
-
+  );
+};
 
 const MessageSentPopup = styled.div`
   position: fixed;
